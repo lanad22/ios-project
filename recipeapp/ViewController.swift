f5b9ba2bab04b5091dc80b67f4c47f2616a33a6e
Lana <do.ng@northeastern.edu>
2023-11-06 22:46:28 -0800

    login screen

--- recipeapp/ViewController.swift
+++ recipeapp/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        let email = emailTextField.text
+        let password = passwordTextField.text
+    }
 }
 
